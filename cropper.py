0c5c960444458427cd0bf2d00aac7678f20ebb7a
BassXVII <steamstoreacc@gmail.com>
2022-08-18 15:48:55 -0500

    Added an example on how to set up cropping dimensions

--- cropper.py
+++ cropper.py
@@ -9,6 +9,15 @@ im1 = Image.open("C:\\Users\\sw-adbass\\Pictures\\Saved Pictures\\Ifunny.jpg")
 print("The dimensions of the current image are: ", im1.size)
 im1.show()
 
+#Sets width and height for manipulation later
+width, height = im1.size
+ 
+# Setting the points for cropped image
+left = 0
+top = height
+right = width
+bottom = 90
+
 
 #Encode an image as base64 to be decoded later.
 with open("C:\\Users\\sw-adbass\\Pictures\\Saved Pictures\\LSC_1327.JPG", "rb") as image:
