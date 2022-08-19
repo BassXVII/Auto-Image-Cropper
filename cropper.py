ecbdbfa7b22898eb00392572f9fcdfb65c1b5fd6
BassXVII <steamstoreacc@gmail.com>
2022-08-19 09:48:27 -0500

    Included listdir to store file names as a list

--- cropper.py
+++ cropper.py
@@ -4,33 +4,44 @@ import base64
 import io
 from PIL import Image     
 
+# import the modules
+import os
+from os import listdir
+
+# get the path or directory
+folder_dir = "C:\\Users\\sw-adbass\\Pictures\\Saved Pictures"
+for images in os.listdir(folder_dir):
+
+	# check if the image ends with png or jpg or jpeg
+	if (images.endswith(".png") or images.endswith(".jpg")\
+		or images.endswith(".jpeg")):
+		# display
+		print(images)
+
 
 #Loop to loop through and crop every image in Pictures folder
-folder_path = "C:\\Users\\sw-adbass\\Pictures\\Saved Pictures\\"
+folder_path = "C:\\Users\\sw-adbass\\Pictures\\Saved Pictures"
 for i in folder_path:
     try:
         img = Image.open(i)
-    except Exception:
-        pass
-
-
-im1 = Image.open("C:\\Users\\sw-adbass\\Pictures\\Saved Pictures\\meme2.png")
-print("The dimensions of the current image are: ", im1.size)
-im1.show()
+        #im1 = Image.open("C:\\Users\\sw-adbass\\Pictures\\Saved Pictures\\meme2.png")
+        print("The dimensions of the current image are: ", img.size)
+        img.show()
 
 #Sets width and height for manipulation later
-width, height = im1.size
+        width, height = img.size
  
 # Setting the points for cropped image
-left = 0
-upper = 0
-right = width
+        left = 0
+        upper = 0
+        right = width
 # Currently, height - 25 seems to be about the size of the watermark. 
-lower = height-25
+        lower = height-25
 
-cropped_Image = im1.crop((left, upper, right, lower))
-cropped_Image.show()
-
+        cropped_Image = img.crop((left, upper, right, lower))
+        cropped_Image.show()
+    except Exception:
+            pass
 
 #Encode an image as base64 to be decoded later.
 with open("C:\\Users\\sw-adbass\\Pictures\\Saved Pictures\\LSC_1327.JPG", "rb") as image:
